ab723d1e8644d7c48d6a573f3538ee5b913e3cb4
ZokaKami <ruffy92@live.com>
2022-11-01 11:30:02 +0100

    Add files via upload

--- base/models.py
+++ base/models.py
@@ -1,8 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-
-class Category(models.model):
-    name = models.CharField(max_length=255, db_index=True)
-    slug = models.SlugField(max_length=255, unique=True)
